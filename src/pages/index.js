d60adfe3a46b01db6badd553ec9557a4d7c2813d
Jared Brown <jaobrown@iu.edu>
2019-06-13 15:37:57 -0400

    Add homepage grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Nav from '../components/Nav'
 import Featured from '../components/Featured'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Home from '../components/Home'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Nav/>
     <Featured />
+    <Home/>
   </Layout>
 )
 
